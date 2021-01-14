20e59453a8e335b8158ee5b4905b2e087c30ab82
Sisifo-G <manuelperdomo1710@icloud.com>
2021-01-14 08:13:54 -0500

    Mostrando datso de presupuesto y restante

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Pregunta from './components/Pregunta';
 import Formulario from './components/Formulario';
+import Listado from './components/Listado';
+import ControlPresupuesto from './components/ControlPresupuesto';
+
 
 function App() {
 
@@ -8,6 +11,29 @@ function App() {
   const [ presupuesto, guardarPresupuesto ] = useState(0);
   const [ restante, guardarRestante ] = useState(0);
   const [ mostrarpregunta, actualizarPregunta ] = useState(true);
+  const [ gastos, guardarGastos ] = useState([]);
+  const [ gasto, guardarGasto ] = useState({});
+  const [ creargasto, guardarCrearGasto ] = useState(false);
+
+  // UseEffect que actualiza el restante
+  useEffect(() => {
+    if(creargasto){
+
+      // Agrega el nuevo presupuesto
+      guardarGastos([
+        ...gastos,
+        gasto
+      ]);
+
+      // Resta del presupuesto actual
+      const presupuestoRestante = restante - gasto.cantidad;
+      guardarRestante(presupuestoRestante);
+
+      // Resetear a False
+      guardarCrearGasto(false);
+    }
+  }, [gasto]);
+
 
   return (
     <div className="container">
@@ -25,11 +51,22 @@ function App() {
           (
             <div className="row">
               <div className="one-half column">
-                <Formulario />
+                <Formulario 
+                  guardarGasto={guardarGasto}
+                  guardarCrearGasto={guardarCrearGasto}
+                />
               </div>
 
               <div className="one-half column">
-                2
+                <Listado 
+                  gastos={gastos}
+                />
+
+                <ControlPresupuesto 
+                  presupuesto={presupuesto}
+                  restante={restante}
+                />
+
               </div>
             </div>
           ) 
